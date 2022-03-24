054fc89c659fc8987fcfc86133d8bcf4c4d899e4
simba0724 <bremaster0806@gmail.com>
2022-03-23 18:57:35 -0700

    check

--- server.js
+++ server.js
@@ -28,50 +28,50 @@ app.use(cors(corsOptions));
 app.use(router);
 
 
-// io.on('connection', function (socket) {
+io.on('connection', function (socket) {
 
-//   socket.on( 'room', function( data ) {
-//     io.sockets.emit( 'room', {
-//     	id_user_sender      : data.id_user_sender,
-//     	id_user_receiver    : data.id_user_receiver,
-//     	id_room             : data.id_room,
-//     	name_receiver       : data.name_receiver,
-//     	name_sender         : data.name_sender,   
-//     	message             : data.message,
-//       type_message        : data.type_message,
-//       file                : data.file,
-//       created_date        : data.created_date,
-//     });
-//   });
+  socket.on( 'room', function( data ) {
+    io.sockets.emit( 'room', {
+    	id_user_sender      : data.id_user_sender,
+    	id_user_receiver    : data.id_user_receiver,
+    	id_room             : data.id_room,
+    	name_receiver       : data.name_receiver,
+    	name_sender         : data.name_sender,   
+    	message             : data.message,
+      type_message        : data.type_message,
+      file                : data.file,
+      created_date        : data.created_date,
+    });
+  });
 
-//   socket.on( 'chat', function( data ) {
-//     io.sockets.emit( 'chat', {
-//     	id_user_sender      : data.id_user_sender,
-//     	id_user_receiver    : data.id_user_receiver,
-//     	id_room             : data.id_room,
-//     	message             : data.message,
-//       type_message        : data.type_message,
-//       file                : data.file,
-//       created_date        : data.created_date,
-//     });
-//   });
+  socket.on( 'chat', function( data ) {
+    io.sockets.emit( 'chat', {
+    	id_user_sender      : data.id_user_sender,
+    	id_user_receiver    : data.id_user_receiver,
+    	id_room             : data.id_room,
+    	message             : data.message,
+      type_message        : data.type_message,
+      file                : data.file,
+      created_date        : data.created_date,
+    });
+  });
 
-//   socket.on( 'ketik', function( data ) {
-//     io.sockets.emit( 'ketik', {
-//       id_room             : data.id_room,
-//       id_user_sender      : data.id_user_sender,
-//     	id_user_receiver    : data.id_user_receiver,
-//     	status              : data.status,
-//     });
-//   });
+  socket.on( 'ketik', function( data ) {
+    io.sockets.emit( 'ketik', {
+      id_room             : data.id_room,
+      id_user_sender      : data.id_user_sender,
+    	id_user_receiver    : data.id_user_receiver,
+    	status              : data.status,
+    });
+  });
 
-//   socket.on( 'online', function( data ) {
-//     io.sockets.emit( 'online', {
-//       id_room             : data.id_room,
-//       id_user_sender      : data.id_user_sender,
-//     	id_user_receiver    : data.id_user_receiver,
-//     	status              : data.status,
-//     });
-//   });
+  socket.on( 'online', function( data ) {
+    io.sockets.emit( 'online', {
+      id_room             : data.id_room,
+      id_user_sender      : data.id_user_sender,
+    	id_user_receiver    : data.id_user_receiver,
+    	status              : data.status,
+    });
+  });
   
-// });
+});
